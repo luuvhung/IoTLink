aa42e13e582a716b175a22aeedc566b371dba632
Luu Hung <luuhung2999@gmail.com>
2021-09-28 11:02:34 +0700

    edit Q9.5

--- Q9.5.cpp
+++ Q9.5.cpp
@@ -37,16 +37,16 @@ int main()
 {
 ClassA *A = new ClassA(10);//m_pVal = 10
 ClassA B(0);//m_pVal = 0
-ClassA *C = &B;//C tro den B
+ClassA *C = &B;
 
 printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
-Todo1(A, &B);
+Todo1(A, &B);//todo1 là hàm swap giá trị của 2 con trỏ=> không ảnh hưởng đến giá trị tại địa chỉ A trỏ đến và B, *C = B nên *C cũng không thay đổi
 printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
-Todo2(*A, B);
+Todo2(*A, B);//tham chiếu *A và B vào hàm todo2 =>swap giá trị *A và B, *C = B
 printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
-Todo3(*A, B);
+Todo3(*A, B);//truyền *A và B vào hàm todo3 và sao chép vào 2 biến A và B của hàm=> sẽ không ảnh hưởng đến *A và B, *C
 printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
-Todo4(A, C);
+Todo4(A, C);//truyền tham chiếu A và C vào hàm todo4, swap 2 giá trị con trỏ này.
 printf("%d %d %d\n", A->m_pVal, B.m_pVal, C->m_pVal);
 system("pause");
 }
